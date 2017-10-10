660bc3e72b5f39cfa7272ac958775080d830ce9b
Javier Delgado <packo6300@gmail.com>
2017-10-10 12:20:26 -0600

    creando pagina de about

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styles: []
+})
+export class FooterComponent {
+    anio:number = new Date().getFullYear();
+  constructor() { }
+}
